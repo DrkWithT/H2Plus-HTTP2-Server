7b16f836bfc08b4dff9032575a0d789d981a372d
drkwitht <derektan3510@gmail.com>
2023-11-22 23:48:53 -0800

    Added file for huffman HPACK utils.

---
+++ src/hpack/huffxcoders.hpp
@@ -0,0 +1,8 @@
+#ifndef HUFFXCODERS_HPP
+#define HUFFXCODERS_HPP
+
+/// @todo Add includes for tables.hpp, octarr.hpp, and symtree.hpp!
+
+/// @todo Declare classes for HuffmanEncoder and HuffmanDecoder... They should deal with BitArray objects.
+
+#endif
