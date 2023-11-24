19020882dbc279d0783470643b82492f46957d78
drkwitht <derektan3510@gmail.com>
2023-11-23 17:13:16 -0800

    Added huffman utils header.

--- src/hpack/huffxcoders.hpp
+++ src/hpack/huffxcoders.hpp
@@ -1,8 +1,39 @@
 #ifndef HUFFXCODERS_HPP
 #define HUFFXCODERS_HPP
 
-/// @todo Add includes for tables.hpp, octarr.hpp, and symtree.hpp!
+#include "hpack/tables.hpp"
+#include "utils/octarr.hpp"
+#include "utils/symtree.hpp"
 
-/// @todo Declare classes for HuffmanEncoder and HuffmanDecoder... They should deal with BitArray objects.
+/**
+ * @brief Encodes an ASCII string into a 1-padded bit sequence according to static Huffman codes. The bits are stored in a BitArray object.
+ * @note For HTTP/2 header Huffman compression.
+ */
+class HuffmanEncoder {
+private:
+    const HuffmanCodePair* huffcodes_ptr; // pointer to statically created huffman code structs
+public:
+    HuffmanEncoder(const HuffmanCodePair* huffcodes);
+    uint32_t encode(BitArray& result, const std::string& text);
+};
+
+/**
+ * @brief Decodes contents from a 1-padded bit sequence according to the static Huffman codes. The refernced bit sequence is a BitArray.
+ * @note For HTTP/2 header Huffman decompression.
+ * @todo Check Tippy2_Server/hpack/huffxcoders.py for general logic.
+ */
+class HuffmanDecoder {
+private:
+    SymbolTree huffcode_tree;
+    const SymbolNode* tree_cursor;
+    bool is_ready;
+
+    void reset_cursor();
+    bool load_huffcode(const HuffmanCodePair* huffcode, uint8_t symbol);
+public:
+    HuffmanDecoder(const HuffmanCodePair* huffcodes);
+    bool setup_valid() const;
+    bool decode(std::string& result, const OctetArray& raw_octets);
+};
 
 #endif
