adda23ffcddec6620922f20b73c0f82574753d0b
drkwitht <derektan3510@gmail.com>
2023-11-23 21:45:19 -0800

    Fixed incorrect signing within some loop conditions.

--- src/huffxcoders.cpp
+++ src/huffxcoders.cpp
@@ -50,7 +50,7 @@ uint32_t HuffmanEncoder::encode(BitArray& result, const std::string& text) {
 
     /// @note If encode_count is on an octet boundary, place padding of 1's until so. This caps an "EOS" symbol (all ones) to the Huffman encoded bitstring.
     if (padding_count != OCTET_BITS) {
-        for (int32_t eos_i = 0U; eos_i < padding_count; eos_i++) {
+        for (uint32_t eos_i = 0U; eos_i < padding_count; eos_i++) {
             if (!result.append(true)) {
                 encode_count = 0U;
                 return encode_count;
@@ -68,7 +68,7 @@ uint32_t HuffmanEncoder::encode(BitArray& result, const std::string& text) {
 HuffmanDecoder::HuffmanDecoder(const HuffmanCodePair* huffcodes) : huffcode_tree {} {
     this->is_ready = true;
 
-    for (uint8_t huffcode_i = 0; huffcode_i < HUFFCODE_PAIR_COUNT; huffcode_i++) {
+    for (uint32_t huffcode_i = 0; huffcode_i < HUFFCODE_PAIR_COUNT; huffcode_i++) {
         if (!load_huffcode(&huffcodes[huffcode_i], huffcode_i)) {
             this->is_ready = false;
             break;
@@ -87,14 +87,19 @@ bool HuffmanDecoder::setup_valid() const {
 }
 
 bool HuffmanDecoder::decode(std::string& result, const OctetArray& raw_octets) {
-    uint32_t data_length = raw_octets.get_length();
+    int32_t data_length = raw_octets.get_length();
+
+    // Do not attempt to decode into invalid OctetArray data e.g length = -1
+    if (data_length < 1) {
+        return false;
+    }
+
     BitArray result_bitstr {OCTET_BITS * data_length};
     bool curr_bit = false;
     bool decode_ok = true;
 
     // Copy all bits of `raw_octets` into `result_bitstr`
-    for (uint32_t copy_i = 0U; copy_i < data_length; copy_i++) {
-        uint8_t bitmask_n = 128;
+    for (int32_t copy_i = 0; copy_i < data_length; copy_i++) {
         const uint8_t temp_octet = raw_octets.get_octet(copy_i);
 
         // Append each octet as 8 bit chunk
@@ -108,7 +113,7 @@ bool HuffmanDecoder::decode(std::string& result, const OctetArray& raw_octets) {
     int32_t result_bit_span = result_bitstr.length();
     uint8_t node_symbol = 0;
 
-    for (uint32_t peek_i = 0; peek_i < result_bit_span; peek_i++) {
+    for (int32_t peek_i = 0; peek_i < result_bit_span; peek_i++) {
         if (!this->tree_cursor) {
             break;
         }
@@ -150,7 +155,7 @@ bool HuffmanDecoder::load_huffcode(const HuffmanCodePair* huffcode, uint8_t symb
     uint32_t bitmask = 1U << code_span;
     bool has_eos = symbol == static_cast<uint8_t>(HUFFCODE_PAIR_COUNT - 1U);
 
-    for (int32_t bit_i = 0; bit_i <= code_span; bit_i++) {
+    for (uint32_t bit_i = 0; bit_i <= code_span; bit_i++) {
         // Decodes bit flag from `temp_huffcode`
         bool bit_flag = (temp_huffcode & bitmask) >> (code_span - bit_i);
         uint8_t curr_symbol = 0;
@@ -178,4 +183,6 @@ bool HuffmanDecoder::load_huffcode(const HuffmanCodePair* huffcode, uint8_t symb
         // Advance forward on bitstring for path
         bitmask >>= 1;
     }
+
+    return true;
 }
--- src/symtree.cpp
+++ src/symtree.cpp
@@ -59,31 +59,31 @@ SymbolNode* SymbolTree::get_root_symbol_node() const {
     return this->root;
 }
 
-bool SymbolTree::put_symbol(uint32_t code, bool is_eos, uint32_t code_length, uint8_t symbol_octet) {
+bool SymbolTree::put_symbol(uint32_t code, uint32_t code_length, uint8_t symbol_octet) {
     bool has_placed = true;
     SymbolNode* temp_cursor = this->root;
     bool has_new_eos = code == HPACK_HUFFCODE_EOS;
     uint32_t code_bitmask = 1 << (code_length - 1);
 
-    for (int i = 0; i < code_length; i++){
+    for (uint32_t i = 0; i < code_length; i++){
         // Track current position in bitstring of Huffman code.
         bool bit_flag = 1 == ((code & code_bitmask) >> (code_length - (i + 1)));
         uint8_t symbol = 0;
 
-        if (i == code_length - 1) {
+        if (i == code_length - 1U) {
             // Prepare to place symbol at end of path.
             symbol = symbol_octet;
         }
 
         if (bit_flag) {
             if (!temp_cursor->left) {
-                temp_cursor->left = symbol_node_create(symbol, is_eos, nullptr, nullptr);
+                temp_cursor->left = symbol_node_create(symbol, has_new_eos, nullptr, nullptr);
             }
 
             temp_cursor = temp_cursor->left;
         } else {
             if (!temp_cursor->right) {
-                temp_cursor->right = symbol_node_create(symbol, is_eos, nullptr, nullptr);
+                temp_cursor->right = symbol_node_create(symbol, has_new_eos, nullptr, nullptr);
             }
 
             temp_cursor = temp_cursor->right;
