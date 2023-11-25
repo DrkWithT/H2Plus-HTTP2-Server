3955aaba2a92e602274240ab9132ce227948c262
drkwitht <derektan3510@gmail.com>
2023-11-25 10:38:56 -0800

    Added simple utility scripting for builds.

---
+++ project.zsh
@@ -0,0 +1,12 @@
+# project.zsh
+# By: Derek Tan
+
+debug_option='y'
+debug_flag=1
+
+# prompt for build mode
+read -p "Do debug build (y/n)? " debug_option
+debug_flag=[[ $debug_option == "y" ]]
+
+# do custom build: either a debug or optimized build
+make DEBUG_BUILD=$debug_flag all && echo -e "\033[0;32mBuild succeeded!\033[0m" || echo -e "\033[0;31mBuild failed!\033[0m"
