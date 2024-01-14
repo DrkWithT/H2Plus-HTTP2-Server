6014383d1ab1bdf9e75bfadd908300ebb574ca4b
drkwitht <derektan3510@gmail.com>
2024-01-14 10:15:49 -0800

    Fixed test script, huffxcoders.cpp

--- run_tests.zsh
+++ run_tests.zsh
@@ -3,22 +3,11 @@
 
 test_prgm_name=''
 
-# Clear debug log to remove obsolete info.
-echo "" > debug_log.txt
-
-# Run all unit test programs and track how many pass / fail.
+# Run all unit test programs and track statuses.
 for prgm in $(find -f ./bin/test_*);
 do
     # Log debug msgs of stdout and stderr. 
-    exec $prgm >> debug_log.txt 2>&1
-
-    # Print test prgm statuses.
-    if [[ $? -eq 0 ]]
-    then
-        echo -e "\033[0;32mPASS: $prgm\033[0m\n"
-    else
-        echo -e "\033[0;31mFAIL: $prgm\033[0m\n"
-    fi
+    $prgm && echo -e "\033[0;32mPASS: $prgm\033[0m\n" || echo -e "\033[0;31mFAIL: $prgm\033[0m\n"
 done
 
-echo -e "\n\033[0;32mAll tests ran: $pass_count passed, $fail_count failed.\033[0m"
+echo -e "\n\033[0;32mAll tests ran.\033[0m"
